5dc0e21cfe42554819c8aee7057efe8829e3de22
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-24 14:44:25 -0400

    Adds volume control and mute/unmute functionality.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// To do:
+// addSong user interface to create Song object with artist and title
+// 
+
 var initialSongs = [
 	new Song("audio-files/meshuggah-clockworks.mp3", "Clockworks", "Meshuggah"),
 	new Song("audio-files/entheos-specific-meaning-in-a-group-of-dots.mp3", "Specific Meaning In A Group Of Dots", "Entheos"),
@@ -15,19 +19,15 @@ function Song(url, title, artist) {
 function Jukebox(songArray) {
 
 	var jukeboxObj = this;
+	var prevVol = 0.5;
 		
 	player = document.getElementById('audio-player');
 	playBtn = document.getElementsByClassName('play-btn')[0];
 	pauseBtn = document.getElementsByClassName('pause-btn')[0];
+	volUpBtn = document.getElementsByClassName('vol-up-btn')[0];
+	volDownBtn = document.getElementsByClassName('vol-down-btn')[0];
+	muteBtn = document.getElementsByClassName('mute-btn')[0];
 	songList = document.getElementsByClassName('song-list')[0];
-
-	this.songs = songArray;
-
-	if (typeof this.songs !== "object") {
-		this.songs = [];
-	}
-	console.log(this.songs);
-
 
 	this.pause = function() {
 		player.pause();
@@ -37,16 +37,53 @@ function Jukebox(songArray) {
 		player.play();
 	}
 
+	this.muteUnmute = function() {
+		if (player.volume === 0) {
+			 unmute();
+		} else {
+			mute();
+		}		
+	}
+
+	unmute = function() {
+		player.volume = prevVol;		
+		muteBtn.innerHTML = "Mute";
+	}
+
+	mute = function() {
+		prevVol = player.volume;
+		player.volume = 0;
+		muteBtn.innerHTML = "Unmute";	
+	}
+
+	this.volumeUp = function() {
+		if (player.volume > 0.95) {
+			player.volume = 1;
+		} else {
+			player.volume += 0.05;		
+		}
+	}
+
+	this.volumeDown = function() {
+		if (player.volume < 0.05) {
+			player.volume = 0;
+		} else {
+			player.volume -= 0.05;
+		}
+	}
+
 	this.addSong = function(songUrl) {
 		this.songs.push(new Song(songUrl));
 		listSongs();
 	}
 
 	listSongs = function() {
-		// for (var k = 0; k < this.songs.length; k++) {
-		// 	var ;
-		// }
-		// songList.innerHTML += ;
+		for (var k = 0; k < jukeboxObj.songs.length; k++) {
+			var song = jukeboxObj.songs[k];
+			var newLi = '<li class="song-list__item">';
+			newLi += song.artist + " - " + song.title;
+			songList.innerHTML += newLi;
+		}
 	}
 
 	loadSong = function(song) {
@@ -56,18 +93,29 @@ function Jukebox(songArray) {
 			player.src = song.url;		
 		}
 	}
+	
+	this.songs = songArray;
+	if (typeof this.songs !== "object") {
+		this.songs = [];
+	}
+	console.log(this.songs);
 
 	this.currentlyPlaying = this.songs[0];
 	loadSong(this.currentlyPlaying);
+	listSongs();
 
 	playBtn.addEventListener("click", this.play);	
 	pauseBtn.addEventListener("click", this.pause);
+	muteBtn.addEventListener("click", this.muteUnmute);
+	volUpBtn.addEventListener("click", this.volumeUp);
+	volDownBtn.addEventListener("click", this.volumeDown);
 }
 
 // Public methods
 // this.pause
 // this.play
 // this.addSong
+// this.muteUnmute
 
 // Public properties:
 // this.player
@@ -78,6 +126,8 @@ function Jukebox(songArray) {
 // Private methods
 // listSongs
 // loadSongs
+// mute
+// unmute
 
 // Private properties
 // playBtn
