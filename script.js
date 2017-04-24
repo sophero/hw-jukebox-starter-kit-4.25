69b7f04a0be0e8b4bbc8c05d30ff8dd2de55f338
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-24 14:11:45 -0400

    Adds loadSong method which changes audio player src.

--- script.js
+++ script.js
@@ -1,47 +1,84 @@
-
-
 var initialSongs = [
-	new Song("Clockworks", "Meshuggah", "audio-files/meshuggah-clockworks.mp3"),
-	new Song("Specific Meaning In A Group Of Dots", "Entheos", "audio-files/entheos-specific-meaning-in-a-group-of-dots.mp3"),
-	new Song("Into Decay", "Meshuggah", "audio-files/meshuggah-into-decay.mp3")
+	new Song("audio-files/meshuggah-clockworks.mp3", "Clockworks", "Meshuggah"),
+	new Song("audio-files/entheos-specific-meaning-in-a-group-of-dots.mp3", "Specific Meaning In A Group Of Dots", "Entheos"),
+	new Song("audio-files/meshuggah-into-decay.mp3", "Into Decay", "Meshuggah")
 	];
 
 var jukebox = new Jukebox(initialSongs);
 
-function Song(title, artist, url) {
+function Song(url, title, artist) {
+	this.url = url;
 	this.title = title;
 	this.artist = artist;
-	this.url = url;
 }
 
 function Jukebox(songArray) {
 
 	var jukeboxObj = this;
 		
-	var player = document.getElementById('audio-player');
-	var playBtn = document.getElementsByClassName('play-btn')[0];
-	var songList = document.getElementsByClassName('song-list')[0];
-
+	player = document.getElementById('audio-player');
+	playBtn = document.getElementsByClassName('play-btn')[0];
+	pauseBtn = document.getElementsByClassName('pause-btn')[0];
+	songList = document.getElementsByClassName('song-list')[0];
 
 	this.songs = songArray;
+
 	if (typeof this.songs !== "object") {
 		this.songs = [];
 	}
+	console.log(this.songs);
 
-	this.currentlyPlaying;
+
+	this.pause = function() {
+		player.pause();
+	}
 
 	this.play = function() {
 		player.play();
 	}
 
 	this.addSong = function(songUrl) {
-		this.songs.push();
+		this.songs.push(new Song(songUrl));
 		listSongs();
 	}
 
 	listSongs = function() {
-
+		// for (var k = 0; k < this.songs.length; k++) {
+		// 	var ;
+		// }
+		// songList.innerHTML += ;
 	}
-	playBtn.addEventListener("click", this.play);	
 
+	loadSong = function(song) {
+		if (typeof song !== "object") {
+			return;
+		} else {
+			player.src = song.url;		
+		}
+	}
+
+	this.currentlyPlaying = this.songs[0];
+	loadSong(this.currentlyPlaying);
+
+	playBtn.addEventListener("click", this.play);	
+	pauseBtn.addEventListener("click", this.pause);
 }
+
+// Public methods
+// this.pause
+// this.play
+// this.addSong
+
+// Public properties:
+// this.player
+// this.songs
+// this.currentlyPlaying
+
+
+// Private methods
+// listSongs
+// loadSongs
+
+// Private properties
+// playBtn
+// songList
